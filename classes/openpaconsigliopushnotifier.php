d0516641c676eb3b8eb42478e996e30cd37b99ac
Luca Realdi <luca.realdi@opencontent.it>
2015-07-31 09:11:59 +0200

    push to mobile

--- classes/openpaconsigliopushnotifier.php
+++ classes/openpaconsigliopushnotifier.php
@@ -27,12 +27,64 @@ class OpenPAConsiglioPushNotifier
 
     public function emit( $identifier, $data )
     {
+        $this->sendToMobile( $identifier, $data );
+
         $this->data = array(
             'identifier' => $identifier,
             'data' => $data
         );
         $this->IsModified = true;
         $this->store();
+    }
+
+    protected function sendToMobile( $identifier, $data )
+    {
+        $url = false;
+        $values = array();
+        $dateTime = new DateTime();
+        $now = $dateTime->format( 'Y-m-d H:i:s' );
+        switch( $identifier )
+        {
+            case 'start_seduta':
+            case 'stop_seduta':
+                $url = 'http://consiglio.u-hopper.com/api/consiglio/seduta';
+                $values = array(
+                    'id' => $data['id'],
+                    'type' => $data['stato'],
+                    'date' => $now
+                );
+            break;
+
+
+            case 'start_punto':
+            case 'stop_punto':
+                $url = 'http://consiglio.u-hopper.com/api/consiglio/punto';
+                $values = array(
+                    'id' => $data['id'],
+                    'type' => $data['stato'],
+                    'date' => $now
+                );
+            break;
+
+            case 'start_votazione':
+            case 'stop_votazione':
+                $url = 'http://consiglio.u-hopper.com/api/consiglio/votazione';
+                $values = array(
+                    'id' => $data['id'],
+                    'type' => $data['stato'],
+                    'date' => $now,
+                    'id_point' => $data['punto_id'],
+                );
+                break;
+        }
+        if ( $url & !empty( $values ) )
+        {
+            $ch = curl_init();
+            curl_setopt( $ch, CURLOPT_URL, $url );
+            curl_setopt( $ch, CURLOPT_POST, 1 );
+            curl_setopt( $ch, CURLOPT_POSTFIELDS, $values );
+            curl_exec( $ch );
+        }
     }
 
     function store()
