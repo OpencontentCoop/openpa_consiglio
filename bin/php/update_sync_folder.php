ef4692d39071e1481a7ea36998b7bbefa548de9d
Luca Realdi <lr@opencontent.it>
2015-09-22 14:20:05 +0200

    script per preparare la cartella di sincronizzazione

---
+++ bin/php/update_sync_folder.php
@@ -0,0 +1,102 @@
+#!/usr/bin/env php
+<?php
+
+// script initializing
+require_once 'autoload.php';
+
+$cli = eZCLI::instance();
+$script = eZScript::instance( array( 'description' => ( "Aggiorna la cartella messa a disposizione per la sincronizzazione con il server locale" ),
+                                     'use-session' => false,
+                                     'use-modules' => true,
+                                     'use-extensions' => true ) );
+
+$script->startup();
+
+$options = $script->getOptions( '[dir:]', '', array( 'dir'  => "Percorso della cartella (default ../<identificatore_sito>_da _sincronizzare" ) );
+
+$script->initialize();
+$script->setUseDebugAccumulators( true );
+
+$user = eZUser::fetchByName( 'admin' );
+eZUser::setCurrentlyLoggedInUser( $user , $user->attribute( 'contentobject_id' ) );
+
+try
+{
+    $identifier = OpenPABase::getCurrentSiteaccessIdentifier();
+    $dirPath = "../{$identifier}_da _sincronizzare";
+
+    if ( !empty( $options['dir'] ) )
+    {
+        $dirPath = $options['dir'];
+    }
+
+    if ( !file_exists( $dirPath ) )
+    {
+        if ( !eZDir::mkdir( $dirPath ) )
+        {
+            throw new Exception( "Non posso creare la cartella $dirPath" );
+        }
+    }
+
+    $dirPath = realpath( $dirPath );
+    $cli->notice( "Cartella di destinazione: $dirPath" );
+
+    $totalSize = 0;
+    function copyBinary( eZBinaryFile $binaryFile )
+    {
+        global $dirPath;
+        global $totalSize;
+        $fileHandler = eZClusterFileHandler::instance( $binaryFile->filePath() );
+        $totalSize += $fileHandler->size();
+        eZFile::create( $binaryFile->attribute( 'filename' ), $dirPath, $fileHandler->fetchContents() );
+    }
+
+    $sedute = OCEditorialStuffHandler::instance( 'seduta' )->fetchItems( array( 'limit' => 100 ) );
+    foreach( $sedute as $seduta )
+    {
+        $cli->notice( $seduta->getObject()->attribute( 'name' ) );
+        foreach( $seduta->attribute( 'documenti' ) as $allegato )
+        {
+            /** @var Allegato $allegato */
+            copyBinary( $allegato->binaryFile() );
+            $cli->notice( " |- [Allegato] " . $allegato->getObject()->attribute( 'name' ) );
+        }
+
+        foreach( $seduta->attribute( 'odg' ) as $punto )
+        {
+            /** @var Punto $punto */
+            if ( $punto->attribute( 'count_documenti' ) > 0 || $punto->attribute( 'count_osservazioni' ) > 0 )
+            {
+                $cli->notice( " |- " . $punto->getObject()->attribute( 'name' ) );
+                foreach ( $punto->attribute( 'documenti' ) as $allegato )
+                {
+                    /** @var Allegato $allegato */
+                    copyBinary( $allegato->binaryFile() );
+                    $cli->notice( "    |- [Allegato] " . $allegato->getObject()->attribute( 'name' ) );
+                }
+                foreach ( $punto->attribute( 'osservazioni' ) as $osservazione )
+                {
+                    /** @var Osservazione $osservazione */
+                    copyBinary( $osservazione->binaryFile() );
+                    $cli->notice( "    |- [Osservazione] " . $osservazione->getObject()->attribute( 'name' ) );
+                }
+            }
+        }
+    }
+
+    function formatBytes($size, $precision = 2)
+    {
+        $base = log($size, 1024);
+        $suffixes = array('', 'k', 'M', 'G', 'T');
+
+        return round(pow(1024, $base - floor($base)), $precision) . $suffixes[floor($base)];
+    }
+    
+    $cli->notice( formatBytes( $totalSize ) );
+    $script->shutdown();
+}
+catch( Exception $e )
+{
+    $script->shutdown( $e->getCode(), $e->getMessage() );
+}
+
