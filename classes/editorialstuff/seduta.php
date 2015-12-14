0701c3ae54d08a3d3f014d334ae039f29fe5efcb
Luca Realdi <luca.realdi@opencontent.it>
2015-12-14 17:36:57 +0100

    set headers

--- classes/editorialstuff/seduta.php
+++ classes/editorialstuff/seduta.php
@@ -1371,7 +1371,17 @@ class Seduta extends OCEditorialStuffPost implements OCEditorialStuffPostFileCon
                     ->setDescription( 'Votazioni ' . $this->getObject()->attribute( 'name' ) );
         $objPHPExcel->getActiveSheet()->fromArray($allData);
 
-        $objWriter = PHPExcel_IOFactory::createWriter( $objPHPExcel, 'Excel5' );
-        $objWriter->save( 'php://output' );
+        $filename = 'votazioni ' .  $this->getObject()->attribute( 'name' ) . '.xls';
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="' . $filename . '"');
+        header('Cache-Control: max-age=0');
+        header('Cache-Control: max-age=1');
+        header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+        header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header ('Pragma: public'); // HTTP/1.0
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+        $objWriter->save('php://output');
+        eZExecution::cleanExit();
     }
 }
